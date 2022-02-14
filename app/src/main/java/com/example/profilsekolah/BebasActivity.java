6eeaacb47bb0173897b2344283e4b18bfef59e8b
diiputri20 <putridini509@gmail.com>
2022-02-14 08:30:22 +0700

    menu profil sekolah

--- app/src/main/java/com/example/profilsekolah/BebasActivity.java
+++ app/src/main/java/com/example/profilsekolah/BebasActivity.java
@@ -1,17 +1,14 @@
 package com.example.profilsekolah;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class BebasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_bebas);
     }
 }
