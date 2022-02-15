a5e5dec29aad2adff8e8566b1139695fa9f95ef3
diiputri20 <putridini509@gmail.com>
2022-02-15 13:49:43 +0700

    profil smpn 18

--- app/src/main/java/com/example/profilsekolah/MainActivity.java
+++ app/src/main/java/com/example/profilsekolah/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.profilsekolah;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void profil(View view) {
+        Intent intent = new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+
+    public void galeri(View view) {
+        Intent intent = new Intent(MainActivity.this, GaleriActivity.class);
+        startActivity(intent);
+    }
+
+    public void guru(View view) {
+        Intent intent = new Intent(MainActivity.this, GuruActivity.class);
+        startActivity(intent);
+    }
+
+    public void ekskul(View view) {
+        Intent intent = new Intent(MainActivity.this, EkskulActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(MainActivity.this, FasilitasActivity.class);
+        startActivity(intent);
+    }
+
+    public void perpus(View view) {
+        Intent intent = new Intent(MainActivity.this, PerpusActivity.class);
+        startActivity(intent);
     }
 }
